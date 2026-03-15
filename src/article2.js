3dc2b85b4c9fafbc159b0d58d349e10cf9773c2d
lohyaoxuan777 <makaujay@zohomail.com>
2026-03-15 17:45:25 +0800

    v0.0.1

---
+++ src/article2.js
@@ -0,0 +1,23 @@
+// background animation
+
+let pastDegree = 360;
+let html = document.querySelector("*");
+html.style.background = `linear-gradient(77deg, #996e9b, #4fff4fff)`;
+setInterval(() => {
+    let hexCodes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f"]
+    let color1 = "";
+    let color2 = "";
+    let degree = 72837;
+    if (pastDegree == 360) {
+        degree = 0;
+    } else {
+        degree = pastDegree + 1;
+    }
+    for (let i=0;i<6;i++) {
+        let chance1 = hexCodes[Math.floor(Math.random() * hexCodes.length)];
+        let chance2 = hexCodes[Math.floor(Math.random() * hexCodes.length)];
+        color1 += chance1;
+        color2 += chance2;
+    };
+    html.style.background = `linear-gradient(${degree}deg, #${color1}, #${color2})`;
+}, 1575);
---
+++ src/index.js
@@ -0,0 +1,50 @@
+let articles = document.querySelectorAll(".news");
+let news1Title = document.querySelector(".news1-title");
+let news1Desc = document.querySelector(".news1-desc");
+let news2Title = document.querySelector(".news2-title");
+let news2Desc = document.querySelector(".news2-desc");
+
+// * CHANGE THIS
+news1Title.textContent = "(12/3/26) C.J. leaks lovers.";
+news1Desc.innerHTML = "Cheah Jason has leaked 6Hs' couples and lovers, more and more gets angry at him. He has <span class=\"read-more\">Read More...</span>";
+news2Title.textContent = "(13/3/26) The Ah Beng's";
+news2Desc.innerHTML = "Today is Ah Beng day! Ah Beng was popularized by Zi Yee, everybody now is saying it lik <span class=\"read-more\">Read More...</span>";
+
+articles.forEach((asdf) => {
+    asdf.addEventListener("click", () => {
+        if (asdf.classList[1] == "news1") {
+            location.href = "article1.html";
+        } else if (asdf.classList[1] == "news2") {
+            location.href = "article2.html";
+        } else if (asdf.classList[1] == "news3") {
+            location.href = "article3.html";
+        } else if (asdf.classList[1] == "news4") {
+            location.href = "article4.html";
+        } else if (asdf.classList[1] == "news5") {
+            location.href = "article5.html";
+        };
+    });
+});
+// background animation
+
+let pastDegree = 360;
+let html = document.querySelector("*");
+html.style.background = `linear-gradient(77deg, #a67f3b, #f7a82d)`;
+setInterval(() => {
+    let hexCodes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f"]
+    let color1 = "";
+    let color2 = "";
+    let degree = 72837;
+    if (pastDegree == 360) {
+        degree = 0;
+    } else {
+        degree = pastDegree + 1;
+    }
+    for (let i=0;i<6;i++) {
+        let chance1 = hexCodes[Math.floor(Math.random() * hexCodes.length)];
+        let chance2 = hexCodes[Math.floor(Math.random() * hexCodes.length)];
+        color1 += chance1;
+        color2 += chance2;
+    };
+    html.style.background = `linear-gradient(${degree}deg, #${color1}, #${color2})`;
+}, 1575);
